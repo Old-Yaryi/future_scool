c87c9541299f4a28c5606d80b30b8db0d2c76d79
Old-Yaryi <kinomir-v@yandex.ru>
2022-07-15 21:04:21 +0300

    'carusel'

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
     slidesToScroll: 1
   });
 });
+const carusel_item = document.querySelectorAll('.faq__items');
+ 
+  for(item of carusel_item){
+    item.addEventListener('click', function(){
+      if(this.classList.contains('active')) {
+        this.classList.remove('active');
+      } else {
+        for(el of carusel_item) {
+        el.classList.remove('active');
+        }
+        this.classList.add('active')
+      }  
+    })
+  }
