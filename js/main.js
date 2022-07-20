8c95e56e3100f0cd8e151e7e982f8da9fd2ad300
Old-Yaryi <kinomir-v@yandex.ru>
2022-07-20 16:56:37 +0300

    'all_1920'

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@ $(document).ready(function(){
   $('.slider').slick({
     
     centerMode: true,
+    // centerPadding: '500px',
     arrows: true,
     dots: false,
     slidesToShow: 3,
     slidesToScroll: 1
   });
 });
-const carusel_item = document.querySelectorAll('.faq__items');
- 
-  for(item of carusel_item){
+
+
+
+const faq_item = document.querySelectorAll('.faq__items');
+  for(item of faq_item){
     item.addEventListener('click', function(){
       if(this.classList.contains('active')) {
         this.classList.remove('active');
       } else {
-        for(el of carusel_item) {
+        for(el of faq_item) {
         el.classList.remove('active');
         }
         this.classList.add('active')
