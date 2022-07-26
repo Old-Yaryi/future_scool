1bfb174514520ddc0ea8c3360fd73b1710df53cb
Old-Yaryi <kinomir-v@yandex.ru>
2022-07-26 15:40:38 +0300

    'who_mobile'

--- js/main.js
+++ js/main.js
@@ -9,9 +9,6 @@ $(document).ready(function(){
     slidesToScroll: 1
   });
 });
-
-
-
 const faq_item = document.querySelectorAll('.faq__items');
   for(item of faq_item){
     item.addEventListener('click', function(){
@@ -25,3 +22,10 @@ const faq_item = document.querySelectorAll('.faq__items');
       }  
     })
   }
+const mobile_menu = document.querySelector('.header__menu');  
+const menu_open = document.querySelector('.header__menu_icon');
+const topmenu_mobile = document.querySelector ('.header__top');
+menu_open.addEventListener('click', function(){
+  mobile_menu.classList.toggle('mobile'),
+  topmenu_mobile.classList.toggle('mobile')
+});
