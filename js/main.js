ec392a01107577f2a2f75f6b6f59285e26c48010
Old-Yaryi <kinomir-v@yandex.ru>
2022-08-01 20:33:44 +0300

    'why_js1'

--- js/main.js
+++ js/main.js
@@ -29,3 +29,36 @@ menu_open.addEventListener('click', function(){
   mobile_menu.classList.toggle('mobile'),
   topmenu_mobile.classList.toggle('mobile')
 });
+
+const why_picture = document.querySelectorAll('.why__items_item-inner');
+const picture_active = document.querySelectorAll('.why__img-img');
+  why_picture.forEach(function (item) {
+    item.addEventListener('click', function () {
+
+      if(this.classList.contains('why__inner-active')) {
+        this.classList.remove('why__inner-active');
+      } else {
+        for(el of why_picture) {
+        el.classList.remove('why__inner-active');
+        }
+        this.classList.add('why__inner-active')
+      }
+
+
+
+      picture_active.forEach(function (item1) {
+        console.log(picture_active)
+        // item1.classList.toggle('img-active')
+        // if(this.classList.contains('img-active')) {
+        //   item1.classList.remove('img-active');
+        // } else {
+        //   for(el of picture_active) {
+        //   el.classList.remove('img-active');
+        //   }
+        //   item1.classList.add('img-active')
+        // }
+      })
+    });
+    
+  });
+  
